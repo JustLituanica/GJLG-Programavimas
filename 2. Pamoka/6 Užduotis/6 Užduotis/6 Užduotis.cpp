b4b82641da1e55255de9389d882b5dadc444f14a
JustLituanica <justinas.launikonis@gmail.com>
2022-10-04 21:31:36 +0300

    6

---
+++ 2. Pamoka/6 Užduotis/6 Užduotis/6 Užduotis.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+
+	system("clear");
+
+	int s, d, c;
+
+	cout << "Ivesk krastine d: ";
+	cin >> d;
+
+	cout << "Ivesk krastine c: ";
+	cin >> c;
+
+	s = d * c;
+
+	cout << "Staciakampio plotas yra " << s;
+
+	return 0;
+}
