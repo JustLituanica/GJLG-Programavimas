0533ab05cbafcfd5fa387687edb09ed997bd696e
JustLituanica <justinas.launikonis@gmail.com>
2022-10-04 21:26:11 +0300

    3 Užduotis

---
+++ 2. Pamoka/3 Užd/3 Užduotis/3 Užduotis.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+
+	system("clear");
+
+	int n, a, vid = 0;
+
+	cout << "Is kiek skaiciu gauti vidurki? ";
+	cin >> n;
+
+	for (int i = 0; n > i; i++) {
+
+		cout << "Ivesk skaiciu: ";
+		cin >> a;
+
+		vid = vid + a;
+	}
+
+	vid = vid / n;
+
+	cout << "Ivestu skaiciu vidurkis yra: " << vid;
+
+	return 0;
+}
