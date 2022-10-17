8ff75a8c4dfec450c20b69d614d9f636c67b8104
JustLituanica <justinas.launikonis@gmail.com>
2022-10-17 22:57:02 +0300

    7 Pamoka

---
+++ 7. Pamoka/4 Užduotis/4 Užduotis.cpp
@@ -0,0 +1,26 @@
+﻿//Lektuvas (20 psl.) Matavimo smulkinimas ir stambinimas
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c, v, m; //kintamieji
+
+    cout << "Kelinta valanda pakilo lektuvas? ";
+    cin >> a;
+
+    cout << "Kelinta minute pakilo lektuvas? ";
+    cin >> b;
+
+    cout << "Kiek minuciu ore praleido lektuvas? ";
+    cin >> c; //ivedimai
+
+    m = (b + c) % 60;
+    v = a + ((b + c)) / 60;
+    v = v % 24; //skaiciuojam atsakyma
+
+    cout << "Lektuvas leisis " << v << " valanda ir " << m << " minute"; //atsakymas
+
+    return 0;
+}
---
+++ 7. Pamoka/5 Užduotis/5 Užduotis.cpp
@@ -0,0 +1,41 @@
+﻿//Skysčių matavimo vienetai (21 psl.)
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int s, g, k, s1, g1, k1;
+    int a, b, c; //kintamieji
+
+    cout << "Saikai: ";
+    cin >> s;
+
+    cout << "Gorčiai: ";
+    cin >> g;
+
+    cout << "Kvortos: ";
+    cin >> k;
+
+    cout << "Kiek saiku vandens ipilama/ispilama? ";
+    cin >> s1;
+
+    cout << "Kiek gorciu vandens ipilama/ispilama? ";
+    cin >> g1;
+
+    cout << "Kiek kvortu vandens ipilama/ispilama? ";
+    cin >> k1; //ivedimai
+
+    c = (k + k1) % 4;
+    b = (g + g1) % 6;
+    a = (k + k1) + (g + g1) / 6;
+
+    cout << a << b << c << endl; //1 ats
+
+    a = (k - k1) % (g - g1) / 6;
+    b = (k + k1) + (g + g1) / 6;
+    c = (k - k1) % 4; //skaiciavimai
+
+    cout << a << b << c << endl; //2 ats
+
+    return 0;
+}
