e24c8387546474e65122a0df91f23eaf48cd369d
JustLituanica <justinas.launikonis@gmail.com>
2022-11-28 22:42:58 +0200

    12 Pamoka

---
+++ 12. Pamoka/1 Uždavinys.cpp
@@ -0,0 +1,20 @@
+﻿//Apskritimo ilgis (27 psl.)
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+const double pi = 3.1414592; //konstanta
+
+int main()
+{
+    double r, c; //kintamieji
+
+    cout << "r: ";
+    cin >> r; //ivedimai
+
+    c = 2 * pi * r; //Skaiciuojam ats
+
+    cout << "c = " << fixed << setprecision(2) << c << endl; //spausdiname ats
+
+    return 0;
+}
