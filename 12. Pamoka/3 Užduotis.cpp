6d2fa931b12cf21ffb441db7e59e05e299d18521
JustLituanica <justinas.launikonis@gmail.com>
2022-11-29 20:18:54 +0200

    12 Pamoka

---
+++ 12. Pamoka/3 Užduotis.cpp
@@ -0,0 +1,26 @@
+﻿//Trikampio ploto skaičiavimas, kai ţinomi visų kraštinių ilgiai. (29 psl.)
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int a, b, c, pp, s; //kintamieji
+
+    cout << "a: ";
+    cin >> a;
+
+    cout << "b: ";
+    cin >> b;
+
+    cout << "c: ";
+    cin >> c; //ivedimai
+
+    pp = (a + b + c) / 2;
+    s = sqrt(pp * (pp- a) * (pp - b) * (pp - c)); //skaiciuojamas ats
+
+    cout << "Trikampio plotas = " << s << " kvadr. vnt" << endl;
+    cout << "Trikampio perimetras = " << pp * 2 << " vnt."; //spausdinamas ats
+
+    return 0;
+}
---
+++ 12. Pamoka/4 Užduotis.cpp
@@ -0,0 +1,39 @@
+﻿//Trikampio ploto skaičiavimas, kai ţinomos visų kraštinių koordinatės. (29 psl.)
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int x1, y1, x2, y2, x3, y3, s, p;
+    int a, b, c; //kintamieji
+
+    cout << "x1: ";
+    cin >> x1;
+
+    cout << "y1: ";
+    cin >> y1;
+
+    cout << "x2: ";
+    cin >> x2;
+
+    cout << "y2: ";
+    cin >> y2;
+
+    cout << "x3: ";
+    cin >> x3;
+
+    cout << "y3: ";
+    cin >> y3; //ivedimai
+
+    a = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    b = sqrt(pow(x3 - x2, 2) + pow(y3 - y2, 2));
+    c = sqrt(pow(x1 - x3, 2) + pow(y1 - y3, 2)); //skaiciuojam atkarpu ilgius
+
+    p = a + b + c; //skaiciuojam perimetra
+    s = 0.25 * sqrt((a + b + c) * (-a + b + c) * (a - b + c) * (a + b - c)); //herono formule plotui apskaiciuoti
+
+    cout << "Trikampio plotas = " << s << " kvadr. vnt." << endl;
+    cout << "Trikampio perimetras = " << p << " vnt."; //spausdinimai
+
+}
