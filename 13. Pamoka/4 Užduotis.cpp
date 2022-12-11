612f1aa202c4e3864337477564ebb4f63e1ddfcb
JustLituanica <justinas.launikonis@gmail.com>
2022-12-11 13:23:27 +0200

    13 Pamoka

---
+++ 13. Pamoka/4 Užduotis.cpp
@@ -0,0 +1,30 @@
+﻿//Kurmio svarstymai (37 psl.)
+#include <iostream>
+using namespace std;
+
+const double gr = 0.5; //konstanta
+
+int main()
+{
+    double g, m, n; //kintamieji
+
+    cout << "g: ";
+    cin >> g;
+
+    cout << "m: ";
+    cin >> m;
+
+    cout << "n: ";
+    cin >> n; //ivedimai
+
+    if ((m * 365.25 * gr * (g / 1000) / 1000) < n)
+    {
+        cout << "Kurmiu vesti Coliuke apsimoka"; //spausdinam ats
+    }
+    else
+    {
+        cout << "Kurmiui vesti Coliukes neapsimoka"; //spausdinam ats
+    }
+
+    return 0;
+}
---
+++ 13. Pamoka/5 Užduotis.cpp
@@ -0,0 +1,34 @@
+﻿//Paskola (37 psl.)
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int s, k, n, t, m; //kintamieji
+
+    cout << "s: ";
+    cin >> s;
+
+    cout << "k: ";
+    cin >> k;
+
+    cout << "n: ";
+    cin >> n;
+
+    cout << "t: ";
+    cin >> t;
+
+    cout << "m: ";
+    cin >> m; //ivedimai
+
+    if (s * n + k < t + m)
+    {
+        cout << "Bankas suteiks paskola"; //spausdiname ats
+    }
+    else
+    {
+        cout << "Bankas paskolos nesuteiks"; //spausdiname ats
+    }
+
+    return 0;
+}
