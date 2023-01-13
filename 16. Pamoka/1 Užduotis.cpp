b633af5ace3dbe05cb95ac32d74efc660f742214
JustLituanica <justinas.launikonis@gmail.com>
2023-01-13 15:36:54 +0200

    16 Pamoka

---
+++ 16. Pamoka/1 Užduotis.cpp
@@ -0,0 +1,42 @@
+﻿//Staigmena (41 psl.)
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double a1, a2, a3, a4, a5;
+    double vid; //kintamieji
+
+    cout << "1. Pazimys: ";
+    cin >> a1;
+
+    cout << "2. Pazimys: ";
+    cin >> a2;
+
+    cout << "3. Pazimys: ";
+    cin >> a3;
+
+    cout << "4. Pazimys: ";
+    cin >> a4;
+
+    cout << "5. Pazimys: ";
+    cin >> a5; //ivedimai
+
+    vid = (a1 + a2 + a3 + a4 + a5) / 5; //vidurkio skaiciavimas
+
+    if (vid > 9)
+    {
+        cout << "Mokinys gaus tris saldainius"; //if sakinys
+    }
+    else if (vid >= 7 && vid <= 9) //&& reiškia 'ir'
+    {
+        cout << "Mokinys gaus du saldainius";
+    }
+    else
+    {
+        cout << "Mokinys gaus viena saldaini"; //else sakiniai
+    }
+
+    return 0;
+}
---
+++ 16. Pamoka/2 Užduotis.cpp
@@ -0,0 +1,32 @@
+﻿//Autobusas (41 psl.)
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t1, t2, t; //kintamieji
+
+    cout << "t1: ";
+    cin >> t1;
+
+    cout << "t2: ";
+    cin >> t2;
+
+    cout << "t: ";
+    cin >> t; //ivedimai
+
+    if (t - t1 > 0)
+    {
+        cout << "Keleivis spes nueiti i autobusa"; //if sakinys
+    }
+    else if (t - t2 > 0)
+    {
+        cout << "Keleivis spes nubegti i autobusa";
+    }
+    else
+    {
+        cout << "Keleivis nespes i autobusa"; //else sakiniai
+    }
+
+    return 0;
+}
