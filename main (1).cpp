c3e18ba4a2a9010828b83f14cf7543ecd25eeb64
Justinas Launikonis <95593194+JustLituanica@users.noreply.github.com>
2023-03-28 12:53:57 +0300

    Kelias į mokyklą (2 Užduotis)

---
+++ main (1).cpp
@@ -0,0 +1,30 @@
+//Kelias į mokyklą (69 psl.)
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int n; //kintamieji
+  int a = 0, b = 0;
+
+  cout << "Įveskite zingsnių kiekį iki mokyklos: ";
+  cin >> n; //ivedam zingsniu skaiciu
+
+  for (int i = 1; i <= n; i++) //for sakinys
+    {
+        if (i % 10 == 0)
+        {
+          a++; //skaiciuojam suplojimus
+        }
+      
+        if (i % 10 == 5)
+        {
+          b++; //skaiciuojam spragtelejimus
+        }
+    }
+
+cout << "Suplojimų bus: " << a << endl;
+  cout << "Spragtelėjimų bus: " << b; //spausdinamas ats
+  
+  return 0;
+}
