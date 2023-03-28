d91a0f8b45feffd35db7bd337d0fe07524a4677a
Justinas Launikonis <95593194+JustLituanica@users.noreply.github.com>
2023-03-28 12:59:10 +0300

    Snaigės už lango (3 užduotis)

---
+++ main (2).cpp
@@ -0,0 +1,22 @@
+//Snaigės uţ lango (69 psl.)
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int k, n, s = 0; //kintamieji
+
+  cout << "Įveskite, kiek snaigių nukrito per pirmąją sekundę ir kelias sekundes snigo: ";
+
+  cin >> k >> n; //ivedimai
+
+  for (int i = 0; i < n; i++)
+    {
+      s = s + k;
+      k = k * 2; //skaiciuojam ats
+    }
+
+  cout << "Iskris " << s << " snaigiu"; //spausdinam ats
+  
+  return 0;
+}
