0ca9529a38769b6cc35bb740cc18920802240248
JustLituanica <justinas.launikonis@gmail.com>
2023-04-19 21:18:39 +0300

    28 Pamoka

---
+++ 28. Pamoka/1 Užduotis.cpp
@@ -0,0 +1,28 @@
+﻿//Sodininkas (75 psl.)
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int m1, m, n, d = 0; //kintamieji
+
+    cout << "m1: ";
+    cin >> m1;
+
+    cout << "m: ";
+    cin >> m;
+
+    cout << "n: ";
+    cin >> n; //ivedimai
+
+    while (n > 0) //...kol medziu daugiau nei 0
+    {
+        n = n - m1; //skaiciuojame likusius medzius
+        m1 = m1 + m; //skaiciuojame medziu kieki kiekviena diena
+        d++; //skaiciuojame dienas
+    }
+
+    cout << "Sodininkas dirbs " << d << " dienas"; //spausdiname ats
+
+    return 0;
+}
